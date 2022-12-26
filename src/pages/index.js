9f264f3cece25d564cb0deb4bf3bf6175e2e1ca9
jahnif <joseph.peha@gmail.com>
2022-12-25 22:18:39 -0800

    Fetching Santa destination data with SWR

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,11 +6,17 @@ import Container from '@components/Container';
 import Map from '@components/Map';
 import Button from '@components/Button';
 
+import useSWR from 'swr';
+
 import styles from '@styles/Home.module.scss';
 
-const DEFAULT_CENTER = [38.907132, -77.036546]
+const DEFAULT_CENTER = [38.907132, -77.036546];
+
+const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function Home() {
+  const { data } = useSWR('https://firebasestorage.googleapis.com/v0/b/santa-tracker-firebase.appspot.com/o/route%2Fsanta_en.json?alt=media&2018b', fetcher);
+  console.log(data);
   return (
     <Layout>
       <Head>
@@ -21,17 +27,12 @@ export default function Home() {
 
       <Section>
         <Container>
-          <h1 className={styles.title}>
-            Next.js Leaflet Starter
-          </h1>
+          <h1 className={styles.title}>Next.js Leaflet Starter</h1>
 
           <Map className={styles.homeMap} width="800" height="400" center={DEFAULT_CENTER} zoom={12}>
             {({ TileLayer, Marker, Popup }) => (
               <>
-                <TileLayer
-                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                  attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-                />
+                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' />
                 <Marker position={DEFAULT_CENTER}>
                   <Popup>
                     A pretty CSS3 popup. <br /> Easily customizable.
@@ -51,5 +52,5 @@ export default function Home() {
         </Container>
       </Section>
     </Layout>
-  )
+  );
 }
